2465aed45ffe5a8ded3aae5a8f914a5aa5db608d
justDaniel2002 <114671803+justDaniel2002@users.noreply.github.com>
2023-06-26 01:19:24 +0700

    update layers and background

---
+++ HyperDrive/Assets/BGloop.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGloop : MonoBehaviour
+{
+    public float speed;
+    public Transform road1;
+    public Transform road2;
+    float _ySize;
+    public bool isStart;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
