bfaa171a8ad924e395cb277113c1d1c05d032aa0
justDaniel2002 <114671803+justDaniel2002@users.noreply.github.com>
2023-06-26 01:32:50 +0700

    set up bg

--- HyperDrive/Assets/BGloop.cs
+++ HyperDrive/Assets/BGloop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGloop : MonoBehaviour
+public class bgloop : MonoBehaviour
 {
     public float speed;
     public Transform road1;
@@ -12,7 +12,11 @@ public class BGloop : MonoBehaviour
 
     private void Awake()
     {
-        
+        _ySize = road1.GetComponent<BoxCollider2D>().size.y * road1.transform.localScale.y;
+
+        road1.transform.position = Vector3.zero;
+
+        road2.transform.position = new Vector3(road1.transform.position.x, road1.transform.position.y + _ySize, 0f);
     }
     // Start is called before the first frame update
     void Start()
