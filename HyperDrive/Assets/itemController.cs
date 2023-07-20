61aa9bbea50a42a564eaae4ac6a774437968d221
justDaniel2002 <114671803+justDaniel2002@users.noreply.github.com>
2023-07-20 17:06:14 +0700

    add item and item controller

---
+++ HyperDrive/Assets/itemController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rgb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rgb.velocity = Vector2.down * speed;
+    }
+}
